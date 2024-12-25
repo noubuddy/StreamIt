0f5c48e37fae3240aed9883916618f4535249c01
noubuddy <anton.nich00@gmail.com>
2024-12-25 23:14:55 +0200

    add winsock2 'alias-functions' (sockets functionality)

--- src/Aliases.h
+++ src/Aliases.h
@@ -2,5 +2,6 @@
 #define ALIASES_H
 
 typedef const char* STRING;
+typedef unsigned long long SOCKET;
 
 #endif // ALIASES_H
--- src/StreamIt.h
+++ src/StreamIt.h
@@ -3,20 +3,26 @@
 
 #include "Aliases.h"
 
-// typedef enum e_connection_status
-// {
-//     CONNECTION_SUCCESS = 0,
-//     CONNECTION_FAILURE
-// } e_connection_status;
-//
-// typedef struct s_connection_props
-// {
-//     int family;
-//     int socket_type;
-//     int protocol;
-//     int flags;
-// } s_connection_props;
+typedef enum e_connection_status
+{
+    CONNECTION_SUCCESS = 0,
+    CONNECTION_FAILURE
+} e_connection_status;
 
-// e_connection_status open_connection(STRING serv_addr, STRING serv_port, s_connection_props& conn_props);
+typedef struct s_connection_props
+{
+    int family;
+    int socket_type;
+    int protocol;
+    int flags;
+} s_connection_props;
+
+typedef struct s_socket_address
+{
+    unsigned short family;
+    char data[14];
+} s_socket_address;
+
+e_connection_status open_connection(STRING serv_addr, STRING serv_port, s_connection_props* conn_props);
 
 #endif // STREAMIT_H
